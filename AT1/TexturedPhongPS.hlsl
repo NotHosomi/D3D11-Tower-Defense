9d97d9a97b38cb5b46248bb84d840ba6f43d2ff3
Hosomi <surendaotoko@gmail.com>
2021-04-24 17:53:16 +0100

    refactor shaders

--- AT1/TexturedPhongPS.hlsl
+++ AT1/TexturedPhongPS.hlsl
@@ -24,16 +24,16 @@ SamplerState splr;
 float4 main(float3 worldPos : Position, float3 n : Normal, float2 tc : Texcoord) : SV_Target
 {
 	// fragment to light vector data
-	const float3 vToL = lightPos - worldPos;
-	const float distToL = length(vToL);
-	const float3 dirToL = vToL / distToL;
+	const float3 incidence = lightPos - worldPos;
+	const float incidence_norm = length(incidence);
+	const float3 incidence_normalized = incidence / incidence_norm;
 	// attenuation
-	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
+	const float att = 1.0f / (attConst + attLin * incidence_norm + attQuad * (incidence_norm * incidence_norm));
 	// diffuse intensity
-	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f,dot(dirToL,n));
+	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f,dot(incidence_normalized,n));
 	// reflected light vector
-	const float3 w = n * dot(vToL,n);
-	const float3 r = w * 2.0f - vToL;
+	const float3 w = n * dot(incidence,n);
+	const float3 r = w * 2.0f - incidence;
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
 	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f,dot(normalize(-r),normalize(worldPos))),specularPower);
 	// final color
